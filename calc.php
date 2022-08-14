857f47cacddf1043e47cc6abb2df28a154469e12
navaneethsujeev <navaneethsujeev99@gmail.com>
2022-08-14 20:05:40 +0530

    style edit

--- calc.php
+++ calc.php
@@ -5,7 +5,11 @@
     <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 </head>
-
+<style>
+    .form-control {
+        width: 20%;
+    }
+</style>
 <?php
 $first_num = $_POST['first_num'];
 $second_num = $_POST['second_num'];
@@ -32,21 +36,21 @@ if (is_numeric($first_num) && is_numeric($second_num)) {
 <body>
     <center>
         <div id="page-wrap">
-        <h1>PHP - Simple Calculator</h1>
+        <h1>PHP Simple Calculator</h1>
         <form action="" method="post" id="quiz-form">
                 <p>
-                    <input type="number" name="first_num" id="first_num" required="required" value="<?php echo $first_num; ?>" /> <b>First Number</b>
+                   First Num  <input type="number" name="first_num" id="first_num" class="form-control" required="required" value="<?php echo $first_num; ?>" /> 
                 </p>
                 <p>
-                    <input type="number" name="second_num" id="second_num" required="required" value="<?php echo $second_num; ?>" /> <b>Second Number</b>
-                </p>
-                <p>
-                    <input readonly="readonly" name="result" value="<?php echo $result; ?>"> <b>Result</b>
+                    Second Num  <input type="number" name="second_num" id="second_num" class="form-control" required="required" value="<?php echo $second_num; ?>" />
                 </p>
                 <input type="submit" class="btn btn-primary" name="operator" value="Add" />
                 <input type="submit" class="btn btn-primary" name="operator" value="Subtract" />
                 <input type="submit" class="btn btn-primary" name="operator" value="Multiply" />
                 <input type="submit" class="btn btn-primary" name="operator" value="Divide" />
+                <p>
+                  Result <input readonly="readonly" name="result" class="form-control" value="<?php echo $result; ?>">
+                </p>
         </form>
         </div>
     </center>
